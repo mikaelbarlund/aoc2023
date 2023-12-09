cc7eb2c42a3643dce33c15a5c0185048480accb8
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2023-12-09 15:49:29 +0200

    Day9 setup

---
+++ src/main/kotlin/Day9.kt
@@ -0,0 +1,8 @@
+fun day9_1(fileContent: List<String>): Any {
+    return 1
+}
+
+
+fun day9_2(fileContent: List<String>): Any {
+    return 1
+}
---
+++ src/test/kotlin/Day9KtTest.kt
@@ -0,0 +1,15 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class Day9KtTest : FunSpec({
+    val fileContent1 = readFileAsLinesUsingUseLines("09_1.txt")
+    test("day9_1") {
+        val result = day9_1(fileContent1)
+        result.shouldBe(2)
+    }
+
+    test("day9_2") {
+        val result = day9_2(fileContent1)
+        result.shouldBe(6)
+    }
+})
