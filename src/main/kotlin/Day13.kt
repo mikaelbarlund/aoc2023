96d5cbf2c32f9ddafa9bb125f9c5126f415d5d72
Mikael Bärlund <mikael.barlund@wunderdog.fi>
2023-12-15 21:51:07 +0200

    Day13 slight refactor

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -17,42 +17,42 @@ tailrec fun findReflectionMemo(
     transposed: Boolean = false,
     original: Pair<Int, Boolean> = Pair(-1, false)
 ): Pair<Int, Boolean> {
-    val result = cache13.getOrPut(listOf(field, loopIndexX, loopIndexY, index, transposed)) {
-        val foo = Pair((index + 1) * if (transposed) 1 else 100, transposed)
-        if (loopIndexY > field.size) {
-            return Pair(-1, transposed)
-        }
-        if (index < field.size - 1 && (loopIndexX < 0 || loopIndexY == field.size)) {
-            if (foo == original)
-                println("wtf")
-            else
-                return foo
-        }
-        if ((index == field.size || loopIndexY == field.size || loopIndexX < 0) && foo != original) {
-            if (transposed) {
-                return Pair(-1, transposed)
-            }
-            val transposedField = transpose(field)
-            return findReflectionMemo(
-                field = transposedField,
-                transposed = true,
-                original = original
-            )
-        } else
-            if (foo != original && index < field.size - 1 && field[loopIndexX] == field[loopIndexY]) {
+    val cached = cache13.getOrPut(listOf(field, loopIndexX, loopIndexY, index, transposed)) {
+        val result = Pair((index + 1) * if (transposed) 1 else 100, transposed)
+        if (loopIndexY > field.size || (index < field.size - 1 && (loopIndexX < 0 || loopIndexY == field.size)) && result != original) {
+            return result
+        } else if ((index == field.size || loopIndexY == field.size || loopIndexX < 0) && result != original) {
+            return if (transposed) {
+                Pair(-1, transposed)
+            } else {
+                val transposedField = transpose(field)
                 return findReflectionMemo(
-                    field,
-                    loopIndexX - 1,
-                    loopIndexY + 1,
-                    index, transposed, original
+                    field = transposedField,
+                    transposed = true,
+                    original = original
                 )
-            } else return findReflectionMemo(
+            }
+        } else if (result != original && index < field.size - 1 && field[loopIndexX] == field[loopIndexY]) {
+            return findReflectionMemo(
                 field,
-                index + 1, index + 2,
-                index + 1, transposed, original
+                loopIndexX - 1,
+                loopIndexY + 1,
+                index,
+                transposed,
+                original
             )
+        } else {
+            return findReflectionMemo(
+                field,
+                index + 1,
+                index + 2,
+                index + 1,
+                transposed,
+                original
+            )
+        }
     }
-    return result
+    return cached
 
 }
 
